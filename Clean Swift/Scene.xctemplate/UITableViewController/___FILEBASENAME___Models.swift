68fb6484de242255d4438741f54e9bf8066e30ea
Michael Tan <tan.michael@mac.com>
2017-05-02 15:54:43 +0700

    Create relative symbolic links so we don’t have to update duplicates of templates.

--- Clean Swift/Scene.xctemplate/UITableViewController/___FILEBASENAME___Models.swift
+++ Clean Swift/Scene.xctemplate/UITableViewController/___FILEBASENAME___Models.swift
@@ -1,17 +1,1 @@
-//
-//  ___FILENAME___
-//  ___PROJECTNAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
-//
-
-import UIKit
-
-struct ___FILEBASENAMEASIDENTIFIER___ {
-    struct Something {
-        struct Request {}
-        struct Response {}
-        struct ViewModel {}
-    }
-}
+../../Models.xctemplate/___FILEBASENAME___Models.swift
--- Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Worker.swift
+++ Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Worker.swift
@@ -1,18 +1,1 @@
-//
-//  ___FILENAME___
-//  ___PROJECTNAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
-//
-
-import UIKit
-
-class ___FILEBASENAMEASIDENTIFIER___Worker {
-
-    // MARK: - Business Logic
-  
-    func doSomeWork() {
-        // NOTE: Do the work
-    }
-}
+../../Worker.xctemplate/___FILEBASENAME___Worker.swift
